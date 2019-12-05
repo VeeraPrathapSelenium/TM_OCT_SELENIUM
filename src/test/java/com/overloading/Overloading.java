1ae1c4b19247fdb2c1cc003de7d769aaaa6ba422
tm <tm@192.168.1.37>
2019-12-05 08:35:12 +0530

    overloading

--- src/test/java/com/overloading/Overloading.java
+++ src/test/java/com/overloading/Overloading.java
@@ -3,8 +3,36 @@ package com.overloading;
 public class Overloading {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Overloading ro=new Overloading();
+	
 	}
+	
+	
+	 void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	
+	
+	public int add(int a,int b,int c)
+	{
+		System.out.println(a+b);
+		return a+b+c;
+	}
+	
+	protected String add(int a,int b,int c,int d)
+	{
+		System.out.println(a+b);
+		return String.valueOf((a+b+c));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
